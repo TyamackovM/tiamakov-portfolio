228fe149399a7b4a516b799959c1d9a6d236086a
TyamackovM <tyamackov@icloud.com>
2023-08-01 14:15:52 +0300

    socials link

--- components/Socials.js
+++ components/Socials.js
@@ -1,5 +1,26 @@
+//* links
+import Link from "next/link";
+
+//* icons
+import { RiInstagramFill, RiLinkedinFill, RiGithubFill, RiTelegramFill } from "react-icons/ri";
+
 const Socials = () => {
-  return <div>Socials</div>;
+  return (
+    <div className="flex items-center gap-x-5 text-lg">
+      <Link href={''} className="hover:text-accent transition-all duration-300">
+        <RiLinkedinFill />
+      </Link>
+      <Link href={''} className="hover:text-accent transition-all duration-300">
+        <RiGithubFill />
+      </Link>
+      <Link href={''} className="hover:text-accent transition-all duration-300">
+        <RiInstagramFill />
+      </Link>
+      <Link href={''} className="hover:text-accent transition-all duration-300">
+        <RiTelegramFill />
+      </Link>
+    </div>
+  );
 };
 
 export default Socials;
